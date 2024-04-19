2e767c23d5ae3feb828a9523199c6749b8085084
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-04-18 22:17:30 -0400

    basic functionalities

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+const form = document.getElementById('bmi-form');
+
+form.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  const age = document.getElementById("age").value;
+  const height = document.getElementById("height").value;
+  const weight = document.getElementById("weight").value;
+  const sex = document.getElementById("")
+})
